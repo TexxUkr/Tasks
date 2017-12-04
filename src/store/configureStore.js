5baca943ac3d88d5bd8872b69941d0f5f3dfe52c
Oleg <oleg.osadchyy@gmail.com>
2017-12-04 14:26:32 -0800

    no redux store avilable in prod

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,15 +2,10 @@ import { createStore, combineReducers } from 'redux'
 import reducers from '../reducers/index'
 
 export default () => {
-  let store
-  if (process.env.NODE_ENV !== 'production') {
-    store = createStore(
-      reducers,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  } else {
-    store = createStore(reducers)
-  }
+  const store = createStore(
+    reducers,
+    ((process.env.NODE_ENV !== 'production') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : undefined),
+  )
   return store
 }
 
